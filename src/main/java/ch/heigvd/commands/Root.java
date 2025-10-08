c0b3aa2e774abae70af5e88a6ef3f70b2135572d
Maikol Da Silva <maikol1180@gmail.com>
2025-10-08 12:13:23 +0200

    Add Comments for method getConfig

--- src/main/java/ch/heigvd/commands/Root.java
+++ src/main/java/ch/heigvd/commands/Root.java
@@ -3,6 +3,8 @@ package ch.heigvd.commands;
 import ch.heigvd.Config;
 import picocli.CommandLine;
 
+import java.io.IOException;
+
 @CommandLine.Command(
     description =
         "Small cli program that manage file structure for students, that allow to create classes and notes",
@@ -18,6 +20,13 @@ public class Root {
       defaultValue = "config.json")
   protected String configFilename;
 
+
+    /**
+     * Get config from filename if it was specified or create a default one
+     *
+     * @return a Config object initialized with the content of the JSON file
+     *     correspond to the Config object structure or a default one if no file found
+     */
   public Config getConfig() {
     Config config;
     try {
