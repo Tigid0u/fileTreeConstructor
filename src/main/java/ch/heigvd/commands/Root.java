fad56a2492de2c2639d8b0dafe52361a86f29c69
Maikol Da Silva <maikol1180@gmail.com>
2025-10-08 11:12:23 +0200

    WIP

--- src/main/java/ch/heigvd/commands/Root.java
+++ src/main/java/ch/heigvd/commands/Root.java
@@ -1,13 +1,13 @@
 package ch.heigvd.commands;
 
-import picocli.CommandLine;
 import ch.heigvd.Config;
+import picocli.CommandLine;
 
 @CommandLine.Command(
-    description = "Small cli program that manage file structure for students, that allow to create classes and notes",
+    description =
+        "Small cli program that manage file structure for students, that allow to create classes and notes",
     version = "1.0.0",
-    subcommands = {
-    },
+    subcommands = {Create_class.class},
     scope = CommandLine.ScopeType.INHERIT,
     mixinStandardHelpOptions = true)
 public class Root {
@@ -18,7 +18,15 @@ public class Root {
       defaultValue = "config.json")
   protected String configFilename;
 
-  public Config getConfigFilename() {
-    return Config.getConfigFromFile(configFilename);
+  public Config getConfig() {
+    Config config;
+    try {
+      config = Config.getConfigFromFile(configFilename);
+    } catch (java.io.IOException e) {
+      System.out.println(
+          "Failed to open Config file : No such file\nCreating a default config file");
+      config = new Config();
+    }
+    return config;
   }
 }
